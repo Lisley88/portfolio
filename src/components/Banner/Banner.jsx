cbd3e1701f8139259cac1cf458baecb41213e1c0
Liaosha <alyosasha@gmail.com>
2024-03-13 09:28:46 +0100

    Ajout de fontawesome

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,14 @@
 import '../Banner/Banner.scss';
-import photo from '../../assets/logo_ls.jpg'
-
+import photo from '../../assets/picture.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHand} from '@fortawesome/free-solid-svg-icons';
 
 function Banner() {
         return (
             <div className='banner'>
                 <div className='banner-box'>
-                    <h3 className='msg-banner'>Bienvenue ! </h3>
+                    <h3 className='msg-banner'>
+                    <FontAwesomeIcon icon={faHand} className='icone' /> Bienvenue ! </h3>
                     <h3>Je suis Liaosha</h3>
                     <h3>Développeuse web junior</h3> 
                 </div>
