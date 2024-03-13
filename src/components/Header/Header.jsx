8c193e5d84d7a572448720986d0edfcc4733f304
Liaosha <alyosasha@gmail.com>
2024-03-13 09:30:41 +0100

    changement de background-color de header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,9 @@
 import { NavLink } from 'react-router-dom';
 import '../Header/Header.scss';
-import logo from '../../assets/logo_ls.jpg';
-
 
 function Header() {
     return (
         <nav className='navbar'>
-            <img src={logo} alt='logo' />
              <div className='navbar__menus'>
                   <NavLink className='navbar__menu' to='/home'>Home</NavLink>
                   <NavLink className='navbar__menu' to='/skills'>Skills</NavLink>
