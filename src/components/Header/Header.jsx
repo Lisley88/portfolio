814e32bb6c511e08bfe2492076ab3daaddf236c0
Liaosha <alyosasha@gmail.com>
2024-03-14 09:38:06 +0100

    switch la page avec ID

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
-import { NavLink } from 'react-router-dom';
 import '../Header/Header.scss';
+import { NavLink } from 'react-router-dom';
+
 
 function Header() {
+    const skills = document.getElementById("skills");
+    const projects = document.getElementById("projects");
+    const contact = document.getElementById("contact");
+
     return (
         <nav className='navbar'>
              <div className='navbar__menus'>
-                  <NavLink className='navbar__menu' to='/home'>Home</NavLink>
-                  <NavLink className='navbar__menu' to='/skills'>Skills</NavLink>
-                  <NavLink className='navbar__menu' to='/:id'>Projects</NavLink>
-                  <NavLink className='navbar__menu' to='/contact'>Contact</NavLink>
+                  <NavLink className='navbar__menu' to="/">Home</NavLink>
+                  <NavLink className='navbar__menu' onClick={() => {skills?.scrollIntoView({behavior: "smooth",});}} >Skills</NavLink>
+                  <NavLink className='navbar__menu' onClick={() => {projects?.scrollIntoView({behavior: "smooth",});}} >Projects</NavLink>
+                  <NavLink className='navbar__menu' onClick={() => {contact?.scrollIntoView({behavior: "smooth",});}} >Contact</NavLink>
              </div>
         </nav>
     )
